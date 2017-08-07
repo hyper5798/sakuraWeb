dcda1f6d6466dd94ed6ece2f95b4e9515885a9b6
jason5798 <jason_huang@gemteks.com>
2017-08-07 13:45:25 +0800

    [0.0.1]Init serverr
    1.Add account admin function

---
+++ routes/index.js
@@ -0,0 +1,327 @@
+﻿var express = require('express');
+var router = express.Router();
+var DeviceDbTools = require('../models/deviceDbTools.js');
+var ListDbTools = require('../models/listDbTools.js');
+var UnitDbTools = require('../models/unitDbTools.js');
+var UserDbTools =  require('../models/userDbTools.js');
+var settings = require('../settings');
+var JsonFileTools =  require('../models/jsonFileTools.js');
+var path = './public/data/finalList.json';
+var unitPath = './public/data/unit.json';
+var selectPath = './public/data/select.json';
+var hour = 60*60*1000;
+var type = 'gps';
+
+function findUnitsAndShowSetting(req,res,isUpdate){
+	UnitDbTools.findAllUnits(function(err,units){
+		var successMessae,errorMessae;
+		var macTypeMap = {};
+
+		if(err){
+			errorMessae = err;
+		}else{
+			if(+units.length>0){
+				successMessae = '查詢到'+units.length+'筆資料';
+			}
+		}
+		req.session.units = units;
+
+		console.log( "successMessae:"+successMessae );
+		res.render('setting', { title: 'Setting',
+			units:req.session.units,
+			user:req.session.user,
+			success: successMessae,
+			error: errorMessae
+		});
+	});
+}
+
+module.exports = function(app) {
+  app.get('/', checkLogin);
+  app.get('/', function (req, res) {
+  	    var now = new Date().getTime();
+		var selectObj = JsonFileTools.getJsonFromFile(selectPath);
+        var user = req.session.user;
+		ListDbTools.findByName('finalist',function(err,lists){
+			if(err){
+				res.render('index', { title: 'Index',
+					success: '',
+					error: err.toString(),
+					finalList:null,
+					type:type,
+					user:user
+				});
+			}else{
+                var finalList = lists[0]['list'];
+				var unitObj = JsonFileTools.getJsonFromFile(unitPath);
+
+				//console.log('finalList :'+JSON.stringify(finalList));
+				if(finalList){
+					var keys = Object.keys(finalList);
+					console.log('Index finalList :'+keys.length);
+					for(var i=0;i<keys.length ;i++){
+						//console.log( i + ') mac : ' + keys[i] +'=>' + JSON.stringify(finalList[keys[i]]));
+						//console.log(i+' result : '+ ((now - finalList[keys[i]].timestamp)/hour));
+						finalList[keys[i]].overtime = true;
+						if( ((now - finalList[keys[i]].timestamp)/hour) < 2 )  {
+							finalList[keys[i]].overtime = false;
+						}
+						finalList[keys[i]].name = '';
+						//console.log(i+' keys[i] : '+ keys[i]);
+						//console.log(i+' unitObj[keys[i]] : '+ unitObj[keys[i]]);
+						if( unitObj[keys[i]] )  {
+							finalList[keys[i]].name = unitObj[keys[i]];
+						}
+					}
+				}else{
+					finalList = null;
+				}
+
+				res.render('index', { title: 'Index',
+					success: null,
+					error: null,
+					finalList:finalList,
+					type:type,
+					user:user,
+					select:selectObj
+				});
+			}
+		});
+  });
+
+  app.get('/evices', checkLogin);
+  app.get('/devices', function (req, res) {
+	var mac = req.query.mac;
+	var type = req.query.type;
+	var date = req.query.date;
+	var option = req.query.option;
+    var user = req.session.user;
+	req.session.type = type;
+	DeviceDbTools.findDevicesByDate(date,mac,Number(0),'desc',function(err,devices){
+		if(err){
+			console.log('find name:'+find_mac);
+			return;
+		}
+		var length = 15;
+		if(devices.length<length){
+			length = devices.length;
+		}
+
+		/*devices.forEach(function(device) {
+			console.log('mac:'+device.date + ', data :' +device.data);
+		});*/
+
+		res.render('devices', { title: 'Device',
+			devices: devices,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString(),
+			type:req.session.type,
+			mac:mac,
+			date:date,
+			option:option,
+			length:length,
+			user:user
+		});
+	});
+  });
+
+  app.get('/login', checkNotLogin);
+  app.get('/login', function (req, res) {
+	req.session.user = null;
+  	var name = req.flash('post_name').toString();
+	var successMessae,errorMessae;
+	console.log('Debug register get -> name:'+ name);
+
+	if(name ==''){
+		errorMessae = '';
+		res.render('user/login', { title: 'Login',
+			error: errorMessae
+		});
+	}else{
+		var password = req.flash('post_password').toString();
+
+		console.log('Debug register get -> password:'+ password);
+		UserDbTools.findUserByName(name,function(err,user){
+			if(err){
+				errorMessae = err;
+				res.render('user/login', { title: 'Login',
+					error: errorMessae
+				});
+			}
+			if(user == null ){
+				//login fail
+				errorMessae = 'The account is invalid';
+				res.render('user/login', { title: 'Login',
+					error: errorMessae
+				});
+			}else{
+				//login success
+				if(password == user.password){
+					req.session.user = user;
+					return res.redirect('/');
+				}else{
+					//login fail
+					errorMessae = 'The password is invalid';
+					res.render('user/login', { title: 'Login',
+						error: errorMessae
+					});
+				}
+			}
+		});
+	}
+  });
+
+  app.post('/login', checkNotLogin);
+  app.post('/login', function (req, res) {
+  	var post_name = req.body.account;
+  	var	post_password = req.body.password;
+  	console.log('Debug login post -> name:'+post_name);
+	console.log('Debug login post -> password:'+post_password);
+	req.flash('post_name', post_name);
+	req.flash('post_password', post_password);
+	return res.redirect('/login');
+  });
+
+  app.get('/logout', function (req, res) {
+    req.session.user = null;
+    req.flash('success', '');
+    res.redirect('/login');
+  });
+  app.get('/account', checkLogin);
+    app.get('/account', function (req, res) {
+
+		console.log('render to account.ejs');
+		var refresh = req.flash('refresh').toString();
+		var myuser = req.session.user;
+		var myusers = req.session.userS;
+		var successMessae,errorMessae;
+		var post_name = req.flash('name').toString();
+
+		console.log('Debug account get -> refresh :'+refresh);
+		UserDbTools.findAllUsers(function (err,users){
+			if(err){
+				errorMessae = err;
+			}
+			if(refresh == 'delete'){
+				successMessae = 'Delete account ['+post_name+'] is finished!';
+			}else if(refresh == 'edit'){
+				successMessae = 'Edit account ['+post_name+'] is finished!';
+			}
+			req.session.userS = users;
+			var newUsers = [];
+			for(var i=0;i<  users.length;i++){
+				//console.log('name : '+users[i]['name']);
+				if( users[i]['name'] !== 'admin'){
+					newUsers.push(users[i]);
+				}
+			}
+			console.log('Debug account get -> users:'+users.length+'\n'+users);
+			
+			//console.log('Debug account get -> user:'+mUser.name);
+			res.render('user/account', { title: 'Account', // user/account : ejs path
+				user:myuser,//current user : administrator
+				users:newUsers,//All users
+				error: errorMessae,
+				success: successMessae
+			});
+		});
+    });
+
+  	app.post('/account', checkLogin);
+  	app.post('/account', function (req, res) {
+  		var	post_name = req.body.postName;
+		var postSelect = req.body.postSelect;
+		console.log('post_name:'+post_name);
+		console.log('postSelect:'+postSelect);
+		var successMessae,errorMessae;
+		req.flash('name',post_name);//For refresh users data
+
+		if(postSelect == ""){//Delete mode
+			UserDbTools.removeUserByName(post_name,function(err,result){
+				if(err){
+					console.log('removeUserByName :'+post_name+ " fail! \n" + err);
+					errorMessae = err;
+				}else{
+					console.log('removeUserByName :'+post_name + 'success');
+					successMessae = successMessae;
+				}
+				UserDbTools.findAllUsers(function (err,users){
+					console.log('Search account count :'+users.length);
+				});
+				req.flash('refresh','delete');//For refresh users data
+				return res.redirect('/account');
+			});
+		}else if(postSelect == "new"){//New account
+			var	password = req.body.password;
+			UserDbTools.findUserByName(post_name,function(err,user){
+				if(err){
+					errorMessae = err;
+					res.render('user/register', { title: '註冊',
+						error: errorMessae
+					});
+				}
+				console.log('Debug register user -> name: '+user);
+				if(user != null ){
+					errorMessae = 'Have the same account!';
+					res.render('user/account', { title: 'Account',
+						error: errorMessae
+					});
+				}else{
+					//save database
+					var level = 1;
+					if(post_name == 'admin'){
+						level = 0;
+					}
+					UserDbTools.saveUser(post_name,password,'',level,function(err,result){
+						if(err){
+							errorMessae = '註冊帳戶失敗';
+							res.render('user/register', { title: 'Account',
+								error: errorMessae
+							});
+						}
+						return res.redirect('/account');
+					});
+				}
+			});
+
+		}else{//Edit modej
+			console.log('postSelect :'+typeof(postSelect) );
+			
+			var json = {enable:(postSelect==="false")?false:true};
+
+			console.log('updateUser json:'+json );
+
+			UserDbTools.updateUser(post_name,json,function(err,result){
+				if(err){
+					console.log('updateUser :'+post_name + err);
+					errorMessae = err;
+				}else{
+					console.log('updateUser :'+post_name + 'success');
+					successMessae = successMessae;
+				}
+				req.flash('refresh','edit');//For refresh users data
+				return res.redirect('/account');
+			});
+		}
+  	});
+};
+
+function checkLogin(req, res, next) {
+  if (!req.session.user) {
+    req.flash('error', 'No Register!');
+    res.redirect('/login');
+  }else
+  {
+	  next();
+  }
+}
+
+function checkNotLogin(req, res, next) {
+  if (req.session.user) {
+    req.flash('error', 'Have login!');
+    res.redirect('back');//返回之前的页面
+  }else
+  {
+	  next();
+  }
+}
---
+++ settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  cookieSecret: 'mydemo',
+  co:'gemteks',
+  dbhost: 'localhost',
+  dbport: 27017,
+  dbpath: 'mongodb://localhost/gemteks',
+  myport: 1884,
+  mytopic:'mqtt',
+  gIotHost:'52.193.146.103',
+  gIotPort: 80,
+  client_Id: '200000109-generic-service',
+  name: '200000109',
+  pw: '57381042',
+  gIoTopic:'client/200000109/200000109-GIOT-MAKER',
+  hour : 60*60*1000
+};
